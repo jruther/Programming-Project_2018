eda110229b9018dc3a979b827275404cf01c0d92
Justin Rutherford <justin.t.rutherford@gmail.com>
2018-04-19 21:40:21 +0100

    Add histogram

--- iris1.py
+++ iris1.py
@@ -3,22 +3,20 @@
 
 import numpy
 
-# Read data file into array
+# Read data file into array (source = Stackoverflow via lecture)
 data = numpy.genfromtxt('data/Iris.csv', delimiter=',')
 
 firstcol = data[:, 0]
-seccol = data[:,1]
+seccol = data[:, 1]
+thirdcol = data[:, 2]
+fourthcol = data[:, 3]
+
 
 print(firstcol)
 print(numpy.min(firstcol))
 import matplotlib.pyplot as mp
 mp.hist(firstcol)
 mp.show()
-
-sepallengthsetosa = data[:51,0]
-print(sepallengthsetosa)
-
-slv = data[51:101,0]
 
 
 
